43960d01734a78013a3211919ec9e5e3c3db9295
MSI\stone <stonepiece@outlook.jp>
2025-10-16 14:38:01 +0900

    絵柄のインポート

--- Assets/Project/Scripts/CardController.cs
+++ Assets/Project/Scripts/CardController.cs
@@ -2,15 +2,44 @@ using UnityEngine;
 
 public class CardController : MonoBehaviour
 {
+    public int[] playerDeck;
+    public int[] enemyDeck;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        CardMaker cardMaker = new();
+        playerDeck = cardMaker.playerDeck;
+        enemyDeck = cardMaker.enemyDeck;
     }
 }
+
+public class CardMaker
+    {
+        int deckNumber = 27;
+        public int[] playerDeck { get; private set; }
+        public int[] enemyDeck { get; private set; }
+
+        public CardMaker()
+        {
+            int[] deck = new int[deckNumber];
+
+            //14はjoker
+            for (int i = 0; i < deck.Length; i++)
+            {
+                deck[i] = (int)Mathf.Ceil((i + 1) / 2);
+            }
+
+            playerDeck = FisherYates(deck);
+            enemyDeck = FisherYates(deck);
+        }
+
+        int[] FisherYates(int[] array)
+        {
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                int RandomNumber = Random.Range(0, i);
+                (array[i], array[RandomNumber]) = (array[RandomNumber], array[i]);
+            }
+            return array;
+        }
+    }
